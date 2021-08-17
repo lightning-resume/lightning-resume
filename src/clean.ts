c4086710813a4ac231596dae689610463fcbd368
oxmanroman <oxmanroman@gmail.com>
2021-08-17 08:55:07 +0200

    fix: using fs in clean

--- src/clean.ts
+++ src/clean.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import path from 'path';
-import shell from 'shelljs';
+import fs from 'fs';
+
 
 const templatesPath = path.resolve('/tmp/linkedin-resume-templates');
-
-shell.exec(`rm -rf ${templatesPath}`);
+fs.rmSync(templatesPath, { recursive: true, force: true });
 console.info(`Cleaned templates directory: ${templatesPath}`);
