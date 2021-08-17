76efb2677b00918756ca2bcbc522ca6bbd7b64bc
jose <joselchecaexpo@gmail.com>
2021-08-17 13:47:34 +0100

    chore(#6): add prefix to commit scope

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  plugins: ['commitlint-plugin-function-rules'],
   rules: {
     'scope-empty': [2, 'never'],
+    'scope-enum': [0], // level: disabled
+    'function-rules/scope-enum': [
+      2,
+      'always',
+      (parsed) => {
+        return parsed.scope.match(/^#+./) ? [true] : [false, `Scope ${parsed.scope}, does not match with the pattern ^#+.`];
+      },
+    ],
   },
 };
