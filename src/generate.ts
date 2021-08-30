426fa2561fcff942602cd8c8e81f318a226cd912
oxmanroman <oxmanroman@gmail.com>
2021-08-30 19:09:26 +0200

    feat(#20): request template json via http

--- src/generate.ts
+++ src/generate.ts
@@ -1,59 +1,64 @@
 #!/usr/bin/env node
 import fs from 'fs-extra';
 import { convertResumeToJSON } from 'linkedin-resume-parser';
+import fetch from 'node-fetch';
 import path from 'path';
 import shell from 'shelljs';
 import yargs from 'yargs/yargs';
-import templates from './templates.json';
 import { downloadRepository } from './utils';
 
-const argv = yargs(process.argv.slice(2))
-  .options({
-    input: {
-      type: 'string',
-      demandOption: true,
-      alias: 'i',
-      description: 'Global path to exported linkedin html file',
-    },
-    output: {
-      type: 'string',
-      demandOption: true,
-      alias: 'o',
-      description: 'Directory where build files will be created',
-    },
-    template: {
-      type: 'string',
-      demandOption: true,
-      alias: 't',
-      choices: Object.keys(templates),
-      description: 'Template name to be used',
-    },
-    config: {
-      type: 'string',
-      demandOption: false,
-      alias: 'c',
-      description: 'Extra configuration json file',
-    },
-    debug: {
-      type: 'boolean',
-      alias: 'd',
-      default: false,
-      description: 'Show debug logs',
-    },
-  })
-  .parseSync();
-
-shell.config.fatal = true;
-shell.config.silent = !argv.debug;
-
-// convert all paths from relative to absolute and normalize them in order to work in every OS
-const templatePath = path.resolve(`/tmp/lightning-resume/${argv.template}`);
-const templateResumePath = path.resolve(`${templatePath}/src/parsed-resume.json`);
-const templateBuildPath = path.resolve(`${templatePath}/build`);
-const inputHtmlPath = path.resolve(argv.input);
-const outputPath = path.resolve(argv.output);
+const templatesUrl = 'https://raw.githubusercontent.com/lightning-resume/lightning-resume/main/src/templates.json';
 
 export async function run(): Promise<void> {
+  console.info('Downloading template list');
+  const templates: { [name: string]: string } = await (await fetch(templatesUrl)).json();
+
+  const argv = yargs(process.argv.slice(2))
+    .options({
+      input: {
+        type: 'string',
+        demandOption: true,
+        alias: 'i',
+        description: 'Global path to exported linkedin html file',
+      },
+      output: {
+        type: 'string',
+        demandOption: true,
+        alias: 'o',
+        description: 'Directory where build files will be created',
+      },
+      template: {
+        type: 'string',
+        demandOption: true,
+        alias: 't',
+        choices: Object.keys(templates),
+        description: 'Template name to be used',
+      },
+      config: {
+        type: 'string',
+        demandOption: false,
+        alias: 'c',
+        description: 'Extra configuration json file',
+      },
+      debug: {
+        type: 'boolean',
+        alias: 'd',
+        default: false,
+        description: 'Show debug logs',
+      },
+    })
+    .parseSync();
+
+  shell.config.fatal = true;
+  shell.config.silent = !argv.debug;
+
+  // convert all paths from relative to absolute and normalize them in order to work in every OS
+  const templatePath = path.resolve(`/tmp/lightning-resume/${argv.template}`);
+  const templateResumePath = path.resolve(`${templatePath}/src/parsed-resume.json`);
+  const templateBuildPath = path.resolve(`${templatePath}/build`);
+  const inputHtmlPath = path.resolve(argv.input);
+  const outputPath = path.resolve(argv.output);
+
   const templateUrl = (templates as { [name: string]: string })[argv.template];
 
   // download selected template
